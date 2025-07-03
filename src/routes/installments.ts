90625a83b05f8474f430c0175b9fb0386edcd2c1
JonathasSC <jonathas.s.cardoso28@gmail.com>
2025-07-03 09:32:17 -0300

    feat: added others installments

--- src/routes/installments.ts
+++ src/routes/installments.ts
@@ -6,20 +6,26 @@ const installmentsRoutes = Router();
 installmentsRoutes.get('/:identification/:contract_num', simulateError, (req: Request, res: Response) => {
   const { identification, contract_num } = req.params;
 
+  const installments = [
+    { numero: 1, dataVencimento: "2024-10-10T00:00:00", situacao: 1 },
+    { numero: 2, dataVencimento: "2024-11-11T00:00:00", situacao: 1 },
+    { numero: 3, dataVencimento: "2024-12-10T00:00:00", situacao: 1 },
+    { numero: 4, dataVencimento: "2025-01-10T00:00:00", situacao: 1 },
+    { numero: 5, dataVencimento: "2025-02-10T00:00:00", situacao: 1 },
+    { numero: 6, dataVencimento: "2025-03-10T00:00:00", situacao: 1 },
+    { numero: 7, dataVencimento: "2025-04-10T00:00:00", situacao: 1 },
+    { numero: 8, dataVencimento: "2025-05-12T00:00:00", situacao: 0 },
+    { numero: 9, dataVencimento: "2025-06-10T00:00:00", situacao: 0 },
+    { numero: 10, dataVencimento: "2025-07-10T00:00:00", situacao: 0 },
+  ];
+
   res.json({
-    situacao: 1,
-    mensagem: "Parcelas encontradas",
-    contador: 1,
-    cpf_cnpj: identification,
-    numeroContrato: contract_num,
-    parcelas: [
-      {
-        numero: 3,
-        vencimento: "2025-07-10",
-        valor: 150.00,
-        situacao: "Em aberto"
-      }
-    ]
+    situacao: 0,
+    mensagem: "",
+    contador: installments.length,
+    cpf_cnpj: Number(identification),
+    numeroContrato: Number(contract_num),
+    parcelas: installments
   });
 });
 
--- src/routes/receipts.ts
+++ src/routes/receipts.ts
@@ -1,19 +1,35 @@
+import path from 'path';
 import { Router, Request, Response } from 'express';
 import simulateError from '../middlewares/simulateError';
-import path from 'path';
 
 const receiptsRoutes = Router();
 
+const installmentMock = [
+  { numero: 8, situacao: 0 },
+  { numero: 9, situacao: 0 },
+  { numero: 10, situacao: 0 },
+];
+
 receiptsRoutes.get('/:identification/:contract_num/:installment_num', simulateError, (req: Request, res: Response) => {
+  const { installment_num, contract_num } = req.params;
+  const numeroInstallment = Number(installment_num);
+
+  const installment = installmentMock.find(p => p.numero === numeroInstallment);
+
+  if (!installment || installment.situacao !== 0) {
+    return res.status(403).json({
+      erro: "A parcela está em aberto há mais de um mês e não pode ser gerada."
+    });
+  }
+
   const filePath = path.resolve(__dirname, '..', 'media', 'boleto.pdf');
 
   res.setHeader('Content-Type', 'application/pdf');
-  res.setHeader('Content-Disposition', `attachment; filename=boleto_${req.params.contract_num}_${req.params.installment_num}.pdf`);
-  
+  res.setHeader('Content-Disposition', `attachment; filename=boleto_${contract_num}_${installment_num}.pdf`);
   res.sendFile(filePath, (err) => {
     if (err) {
-      console.error('Erro ao enviar o PDF:', err);
-      res.status(500).send('Erro ao baixar o boleto.');
+      console.error('Erro ao enviar boleto:', err);
+      res.status(500).send('Erro ao gerar boleto.');
     }
   });
 });
